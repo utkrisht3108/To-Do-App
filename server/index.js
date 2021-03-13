442ce1923ab06f141fcc64081de0c93c9e90d130
utkrisht3108 <utkrisht2001.sharma@gmail.com>
2021-03-13 18:52:53 +0530

    backend added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const todoRoutes = express.Router();
 const PORT = 8000;
 
-let Todo = require('./model/todoModel');
+const Todo = require('./model/todoModel');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,3 +17,56 @@ mongoose.connect('mongodb://localhost:27017/todoDB', {
 });
 console.log('hogaya');
 
+todoRoutes.route('/').get((req, res) => {
+  console.log('ok');
+  Todo.find((err, el) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(el);
+    }
+  });
+});
+
+todoRoutes.route('/add').post((req, res) => {
+  const todo = new Todo(req.body);
+  todo
+    .save()
+    .then((todo) => {
+      res.status(200).json({ todo: 'todo added successfully' });
+    })
+    .catch((err) => {
+      res.status(400).send('adding new todo failed');
+    });
+});
+
+todoRoutes.route('/:id').get((req, res) => {
+  const id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route('/update/:id').post((req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (!todo) res.status(404).send('data is not found');
+    else todo.description = req.body.description;
+    todo.responsible = req.body.responsible;
+    todo.priority = req.body.priority;
+    todo.completed = req.body.completed;
+    todo
+      .save()
+      .then((todo) => {
+        res.json('Todo updated');
+      })
+      .catch((err) => {
+        res.status(400).send('Update not possible');
+      });
+  });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
